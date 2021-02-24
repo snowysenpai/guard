994dca3ba6dc7d243bd8c43b8196b3db068968c0
snowytr <77892547+snowytr@users.noreply.github.com>
2021-02-24 09:45:35 +0300

    Update hd.js

--- komutlar/hd.js
+++ komutlar/hd.js
@@ -3,9 +3,9 @@ const weather = require('weather-js');
 
 module.exports.run = (client, message, args) => {
   weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
-      if (err) message.channel.cache.send(err);
+      if (err) message.channel.send(err);
       if (result === undefined || result.length === 0) {
-          message.channel.cache.send('🚫 **Lokasyon/Bölge Bulunamadı...**')
+          message.channel.send('🚫 **Lokasyon/Bölge Bulunamadı...**')
           return;
       }
       var current = result[0].current;
