b444f79875bfa3aa6091eb79d0633c2d4d598f47
snowytr <77892547+snowytr@users.noreply.github.com>
2021-02-24 09:14:33 +0300

    Add files via upload

---
+++ komutlar/info.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const moment = require("moment");
+const os = require('os');
+require("moment-duration-format");
+exports.run = async (bot, message, args) => {
+   const seksizaman = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+   const istatistikler = new Discord.MessageEmbed()
+  .setColor('#00f5ff')
+  .setFooter('Shavian BOT Stats', bot.user.avatarURL)
+  .addField("» **Botun Sahibi**", "<@!425336949516140547>")
+  .addField("» **Botun Yapımcısı**", "<@!295791590096502784>")
+  .addField("» **RAM Kullanımı**", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true)  
+  .addField("» **Çalışma Zamanı**", seksizaman)
+  .addField("» **Kullanıcılar**" , bot.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
+  .addField("» **Sunucular**", bot.guilds.cache.size.toLocaleString(), true)
+  .addField("» **Kanallar**", bot.channels.cache.size.toLocaleString(), true)
+  .addField("» **Discord.JS Sürümü**", "v"+Discord.version, true)
+  .addField("» **Node.JS Sürümü**", `${process.version}`, true)
+  .addField("» **Ping**", bot.ws.ping+" ms", true)
+  .addField("» **İşlemci**", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
+  .addField("» **Bit**", `\`${os.arch()}\``, true)
+  .addField("» **OS**", `\`\`${os.platform()}\`\``)
+  .setDescription("Bu Bot Snowy Tarafından Hazırlanmıştır") 
+ return message.channel.send(istatistikler);
+  };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 3
+};
+
+exports.help = {
+  name: "stats",
+  description: "Botun Anlık İstatistiklerini Gösterir.",
+  usage: "istatistik"
+};
